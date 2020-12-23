f233e108efbcdb0d6af1f2428490d376ce0f346d
Manar Hasan <manar.hasan62@gmail.com>
2020-12-23 10:38:53 +0200

    first day

--- TPL/TPL_app/urls.py
+++ TPL/TPL_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('parent-profile', views.parent_profile),
+    path('teacher-profile', views.teacher_profile),
+    path('details', views.details),
+    path('home', views.home)
 ]
--- TPL/TPL_app/views.py
+++ TPL/TPL_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "signup_login.html")
+
+
+def parent_profile(request):
+    return render(request, "parent_profile.html")
+
+
+def teacher_profile(request):
+    return render(request, "teacher_profile.html")
+
+
+def details(request):
+    return render(request, "schedule_day.html")
+
+
+def home(request):
+    return render(request, "home_page.html")
