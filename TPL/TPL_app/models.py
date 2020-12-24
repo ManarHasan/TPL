2e64b1a73bd43e78cdfb76f7940eac8c0d212d54
Manar Hasan <manar.hasan62@gmail.com>
2020-12-24 19:23:16 +0200

    something

--- TPL/TPL_app/models.py
+++ TPL/TPL_app/models.py
@@ -4,116 +4,26 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
 
 
-class Teacher_Manager(models.Manager):
-    def basic_validator(self, postData):
-        errors = {}
-        if self.validate_text(postData['first_name']) == 0:
-            errors["first_name"] = "You must enter a name"
-        if self.validate_text(postData['first_name']) == 1:
-            errors["fname_length"] = "first name is too short"
-        if self.validate_text(postData['last_name']) == 0:
-            errors["last_name"] = " you must enter a last name"
-        if self.validate_text(postData["last_name"]) == 1:
-            errors["lname_length"] = " last name is too short"
-        if self.validate_text(postData['occupation']) == 0:
-            errors["occupation"] = "You must enter your occupation information"
-        if self.validate_text(postData['occupation']) == 1:
-            errors["occupation_length"] = "the data you entered is too short"
-        if self.validate_text(postData['specialization']) == 0:
-            errors["specialization"] = "You must enter your specialization info"
-        if self.validate_text(postData['specialization']) == 1:
-            errors["specialization_length"] = "the data you entered is too short"
-        return errors
-
-    def email_validator(self, postData):
-        errors = {}
-        EMAIL_REGEX = re.compile(
-            r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        if not EMAIL_REGEX.match(postData['email']):
-            errors['email'] = "Invalid email address!"
-        return errors
-
-
 class Teacher(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     occupation = models.CharField(max_length=255)
     specialization = models.CharField(max_length=255)
     education = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = Teacher_Manager()
-
-    def validate_text(self, text, min_length=2):
-        if text.isalpha == False:
-            return 0
-        elif len(text) < min_length:
-            return 1
-        elif len(text) > min_length:
-            return 2
-
-
-class Parent_Manager(models.Manager):
-    def validator(self, postData):
-        errors = {}
-        if self.validate_text(postData['fname']) == 1:
-            errors["fname"] = "This name is too short!"
-        if self.validate_text(postData['fname']) == 0:
-            errors["fname_letters"] = "A name must be Abc characters only! Sorry Elon Musk!"
-        if self.validate_text(postData['lname']) == 1:
-            errors["lname"] = "This name is too short!"
-        if self.validate_text(postData['lname']) == 0:
-            errors["lname_letters"] = "A name must be Abc characters only!"
-        if self.validate_email(postData['email']) == 0:
-            errors["email"] = "Invalid email"
-        if self.validate_email(postData['email']) == 1:
-            errors["email_exists"] = "This email already exists"
-        if self.validate_text(postData['password'], max_length=8) == 1:
-            errors["password"] = "Password is too short!"
-        if self.validate_text(postData['occupation'], max_length=5) == 1:
-            errors["occupation"] = "Occupation must be at least 5 charcters"
-        return errors
 
 
 class Parent(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     occupation = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = Parent_Manager()
-
-    def validate_text(self, text, min_length=2):
-        if text.isalpha == False:
-            return 0
-        elif len(text) < min_length:
-            return 1
-        elif len(text) > min_length:
-            return 2
-
-    def validate_email(self, email):
-        regex = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9._-]+.[a-zA-Z]+$')
-        if re.search(regex, email):
-            if not self.is_duplicate_email(email):
-                return 2
-            return 1
-        return 0
-
-    def is_duplicate_email(self, email):
-        parent = Parent.objects.filter(email=email).values()
-        if len(parent):
-            return True
-        return False
-
-
-class Child_Manager(models.Manager):
-    def validator(self, postData):
-        errors = {}
-        if self.validate_text(postData['fname']) == 1:
-            errors["fname"] = "This name is too short!"
-        if self.validate_text(postData['fname']) == 0:
-            errors["fname_letters"] = "A name must be Abc characters only! Sorry Elon Musk!"
-        return errors
 
 
 class Child(models.Model):
@@ -125,26 +35,9 @@ class Child(models.Model):
     parent = models.ForeignKey(
         Parent, related_name="children", on_delete=models.CASCADE)
     lessons = models.ManyToManyField(
-        Teacher, through="Lesson")
+        Teacher, through="Lesson", related_name="lessons")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = Child_Manager()
-
-    def lastName(self, postData):
-        self.last_name = postData['lname']
-
-
-class Lesson_Manager(models.Manager):
-    def validator(self, postData):
-        errors = {}
-        if (len(postData['title']) < 2):
-            errors['title'] = "title should have at least many characters"
-        elif not(NAME_REGEX.match(postData['title'])):
-            errors['title'] = "title: Invalid title"
-
-        if (len(postData['description ']) < 5):
-            errors['description'] = "description  should have at least many characters"
-        return errors
 
 
 class Lesson(models.Model):
@@ -152,8 +45,132 @@ class Lesson(models.Model):
     description = models.TextField()
     max_students = models.IntegerField()
     price = models.IntegerField()
-    child = models.ForeignKey(Child, on_delete=models.CASCADE)
+    style = models.CharField(max_length=255, null=True)
+    child = models.ForeignKey(
+        Child, on_delete=models.CASCADE, blank=True, null=True)
+    time = models.DateTimeField(null=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = Lesson_Manager()
+
+
+def add_parent(postData, pw_hash):
+    user = Parent.objects.create(
+        first_name=postData['fname'], last_name=postData['lname'], occupation=postData['occupation'], email=postData['email'], password=pw_hash)
+    return user
+
+
+def add_child(postData, user_id):
+    child = Child.objects.create(first_name=postData['child_name'], last_name=postData['lname'],
+                                 age=postData['age'], gender=postData['gender'], grade=postData['grade'], parent=Parent.objects.get(id=user_id))
+    return child
+
+
+def add_teacher(postData, pw_hash):
+    user = Teacher.objects.create(
+        first_name=postData['fname'], last_name=postData['lname'], occupation=postData['occupation'], email=postData['email'], password=pw_hash, specialization=postData['specialization'], education=postData['education'])
+    return user
+
+
+def add_lesson(postData, id):
+    time = "2020, "+str(postData['time'])+", " + \
+        str(postData['day'])+", " + \
+        str(postData['month'])+", 0, 0, tzinfo=<UTC>"
+    lesson = Lesson.objects.create(title=postData['title'], description=postData['description'], max_students=1,
+                                   price=postData['price'], style=postData['style'], time=time, teacher=Teacher.objects.get(id=id))
+    return lesson
+
+
+def get_parent(email):
+    user = Parent.objects.filter(email=email)
+    return user[0]
+
+
+def get_teacher(email):
+    user = Teacher.objects.filter(email=email)
+    return user[0]
+
+
+def is_duplicate_email(email):
+    parent = Parent.objects.filter(email=email).values()
+    if len(parent):
+        return True
+    return False
+
+
+def validate_text(text, min_length=2):
+    if text.isalpha == False:
+        return 0
+    elif len(text) < min_length:
+        return 1
+    elif len(text) > min_length:
+        return 2
+
+
+def validate_email(email):
+    regex = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]+$')
+    if re.search(regex, email):
+        if not is_duplicate_email(email):
+            return 2
+        return 1
+    return 0
+
+
+def teacher_validator(postData):
+    errors = {}
+    if validate_text(postData['fname']) == 0:
+        errors["first_name"] = "You must enter a name"
+    if validate_text(postData['fname']) == 1:
+        errors["fname_length"] = "first name is too short"
+    if validate_text(postData['lname']) == 0:
+        errors["last_name"] = "You must enter a last name"
+    # if validate_text(postData["lname"]) == 1:
+    #     errors["lname_length"] = "last name is too short"
+    if validate_text(postData['occupation']) == 0:
+        errors["occupation"] = "You must enter your occupation information"
+    if validate_text(postData['occupation']) == 1:
+        errors["occupation_length"] = "the data you entered is too short"
+    if validate_text(postData['specialization']) == 0:
+        errors["specialization"] = "You must enter your specialization info"
+    if validate_text(postData['specialization']) == 1:
+        errors["specialization_length"] = "the data you entered is too short"
+    return errors
+
+
+def parent_validator(postData):
+    errors = {}
+    if validate_text(postData['fname']) == 1:
+        errors["fname"] = "This name is too short!"
+    if validate_text(postData['fname']) == 0:
+        errors["fname_letters"] = "A name must be Abc characters only! Sorry Elon Musk!"
+    if validate_text(postData['lname']) == 1:
+        errors["lname"] = "This name is too short!"
+    if validate_text(postData['lname']) == 0:
+        errors["lname_letters"] = "A name must be Abc characters only!"
+    if validate_email(postData['email']) == 0:
+        errors["email"] = "Invalid email"
+    if validate_email(postData['email']) == 1:
+        errors["email_exists"] = "This email already exists"
+    if validate_text(postData['password'], min_length=8) == 1:
+        errors["password"] = "Password is too short!"
+    if validate_text(postData['occupation'], min_length=5) == 1:
+        errors["occupation"] = "Occupation must be at least 5 charcters"
+    return errors
+
+
+def child_validator(postData):
+    errors = {}
+    if validate_text(postData['fname']) == 1:
+        errors["fname"] = "This name is too short!"
+    if validate_text(postData['fname']) == 0:
+        errors["fname_letters"] = "A name must be Abc characters only! Sorry Elon Musk!"
+    return errors
+
+
+def lesson_validator(postData):
+    errors = {}
+    if len(postData['title']) < 2:
+        errors['title'] = "title should have at least many characters"
+    if len(postData['description ']) < 5:
+        errors['description'] = "description  should have at least many characters"
+    return errors
--- TPL/TPL_app/views.py
+++ TPL/TPL_app/views.py
@@ -1,16 +1,130 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Parent, Child, Lesson, Teacher
+from . import models
+import datetime
+from django.contrib import messages
+import bcrypt
 
 
-def index(request):
+def login_registration(request):
     return render(request, "signup_login.html")
 
 
-def parent_profile(request):
-    return render(request, "parent_profile.html")
+def register(request):
+    if request.POST['option'] == "parent":
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(
+            password.encode(), bcrypt.gensalt()).decode()
+        errors = models.parent_validator(
+            request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+                return redirect('/')
+        user = models.add_parent(request.POST, pw_hash)
+        child = models.add_child(request.POST, user.id)
+        if 'user_id' not in request.session:
+            request.session['user_id'] = user.id
+            request.session['first_name'] = user.first_name
+            request.session['last_name'] = user.last_name
+            return redirect('/parent-profile/'+str(request.session['user_id']))
+    if request.POST['option'] == "teacher":
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(
+            password.encode(), bcrypt.gensalt()).decode()
+        errors = models.parent_validator(
+            request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+                return redirect('/')
+        user = models.add_teacher(request.POST, pw_hash)
+        if 'user_id' not in request.session:
+            request.session['user_id'] = user.id
+            request.session['first_name'] = user.first_name
+            request.session['last_name'] = user.last_name
+            return redirect('/teacher-profile/'+str(request.session['user_id']))
 
 
-def teacher_profile(request):
-    return render(request, "teacher_profile.html")
+def login(request):
+    if request.POST['options'] == "parent":
+        email = request.POST['email']
+        user = models.get_parent(email)
+        if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+            if user is not None:
+                if 'user_id' not in request.session:
+                    request.session['user_id'] = user.id
+                    request.session['first_name'] = user.first_name
+                    request.session['last_name'] = user.last_name
+                return redirect('/parent-profile/'+str(request.session['user_id']))
+    if request.POST['options'] == "teacher":
+        email = request.POST['email']
+        user = models.get_teacher(email)
+        if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+            if user is not None:
+                if 'user_id' not in request.session:
+                    request.session['user_id'] = user.id
+                    request.session['first_name'] = user.first_name
+                    request.session['last_name'] = user.last_name
+                return redirect('/teacher-profile/'+str(request.session['user_id']))
+    return redirect('/')
+
+
+def logout(request):
+    del request.session['user_id']
+    del request.session['first_name']
+    del request.session['last_name']
+    return redirect('/')
+
+
+def parent_profile(request, id):
+    # if ['user_id'] not in request.session:
+    #     messages.error(request, "You must be logged in to view this page")
+    parent = Parent.objects.get(id=id)
+    children = parent.children.all()
+    all_lessons = []
+    for child in children:
+        all_lessons.append(Lesson.objects.filter(child=child.id))
+
+    context = {
+        "parent": parent,
+        "children": children,
+        "all_lessons": all_lessons
+    }
+    return render(request, "parent_profile.html", context)
+
+
+def teacher_profile(request, id):
+    teacher = Teacher.objects.get(id=id)
+    lessons = teacher.lessons.all()
+    all_lessons = []
+    all_times = []
+    for child in lessons:
+        all_lessons.append(Lesson.objects.filter(
+            teacher=teacher, child=child.id))
+    for i in all_lessons:
+        for time in i:
+            all_times.append(time.created_at)
+    context = {
+        "teacher": teacher,
+        "lessons": lessons,
+        "all_lessons": all_lessons
+    }
+    return render(request, "teacher_profile.html", context)
+
+
+def add_lesson(request, id):
+    teacher = Teacher.objects.get(id=id)
+    # errors = models.lesson_validator(
+    #     request.POST)
+    # if len(errors) > 0:
+    #     for key, value in errors.items():
+    #         messages.error(request, value)
+    #         return redirect('/')
+    lesson = models.add_lesson(request.POST, id)
+    print('-'*40)
+    print(lesson.time)
+    return ('/teacher-profile/'+str(id))
 
 
 def details(request):
